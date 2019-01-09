9fed47a41dee0ed1e6c18ac8ecedf4b9f3dc17d5
xb204417 <roliveira.victor@gmail.com>
2019-01-09 21:59:11 -0200

    added Credentials validation TODO

--- app/src/main/java/stonetree/com/stntech/utils/CredentialUtils.java
+++ app/src/main/java/stonetree/com/stntech/utils/CredentialUtils.java
@@ -3,14 +3,17 @@ package stonetree.com.stntech.utils;
 public class CredentialUtils {
 
     public static boolean isNotValidCpf(String username) {
+        //TODO - Implement CPF Validation
         return false;
     }
 
     public static boolean isNotValidEmail(String username) {
+        //TODO - Implement Email Validation
         return false;
     }
 
     public static boolean isNotValidPassword(String password) {
+        //TODO - Implement Password Validation
         return false;
     }
 
